131edf33cce35277e4cfebf3abd02b06ca2940a0
antoniobariela-svg <antoniobariela@gmail.com>
2026-03-18 16:09:08 +0700

    Add files via upload

---
+++ constants/theme.js
@@ -0,0 +1,40 @@
+export const COLORS = {
+  cream: '#FDF6EC',
+  warmWhite: '#FFF9F2',
+  brownDark: '#3B1F0A',
+  brownMid: '#7C4A1E',
+  brownLight: '#C17F3E',
+  orange: '#E8622A',
+  orangeLight: '#F4A461',
+  tan: '#E8D5B0',
+  tanDark: '#D4B896',
+  textDark: '#2C1A0E',
+  textMid: '#6B4226',
+  textLight: '#A07850',
+  white: '#FFFFFF',
+  successGreen: '#4CAF50',
+  warningOrange: '#FF9800',
+  errorRed: '#F44336',
+};
+
+export const FONTS = {
+  display: 'serif',
+  body: 'System',
+};
+
+export const SHADOWS = {
+  card: {
+    shadowColor: '#3B1F0A',
+    shadowOffset: { width: 0, height: 6 },
+    shadowOpacity: 0.12,
+    shadowRadius: 16,
+    elevation: 6,
+  },
+  soft: {
+    shadowColor: '#3B1F0A',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.06,
+    shadowRadius: 8,
+    elevation: 2,
+  },
+};
